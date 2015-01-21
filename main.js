8874dd9db938b2013a29a981df7f32ab6b4028ee
TheNoobCat <ivar.myrberg@gmail.com>
2015-01-21 21:04:10 +0100

    Creating another square

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-// The main javascript code will go here
-// Test
-//Test2
+function draw() {
+  var canvas = document.getElementById('mainCanvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillRect(250,250,10,10);
+    ctx.clearRect(450,45,60,60);
+    ctx.strokeRect(50,50,50,50);
+    
+  }
+}
+draw();
