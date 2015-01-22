d0b188af61ff76cf9aa4fe181a3bb6bb0129b1e1
louishudd <louishuddleston13@yahoo.co.uk>
2015-01-22 18:05:39 +0000

    Revert "Cleaned up and added clearRect to stop ghost trail"
    This reverts commit d07094248212be09e7884590931aced7ffdd3492.

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-var canvas = document.getElementById('mainCanvas');
-var ctx = canvas.getContext('2d');
-
 var square1 = {
     x: 200, 
     y: 200,
     width: 20,
     height: 20
 };
-
-setInterval(function(){
-    square1.x = square1.x + 1;
-    ctx.clearRect(0,0,600,500);
+function draw() {
+  var canvas = document.getElementById('mainCanvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
     ctx.fillRect(square1.x, square1.y, square1.width, square1.height);
+
+  }
+}
+draw();
+setInterval(function(){	
+    draw();
+    square1.x = square1.x + 1;
 },1000/30);
 
